40995b84fa5b6de30989d1a8666dc0e11efc0564
DominickLidwin <dlidwin@xpanxion.com>
2022-06-14 15:51:53 -0400

    dataTypes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+Scanner input = new Scanner (System.in);
+int days;
+int hours;
+int minutes;
+int seconds;
+int weeks;
+System.out.println("enter numbers of weeks");
+weeks = input.nextInt();
+days = weeks * 7;
+System.out.println(days +" days.");
+hours = weeks * 168;
+System.out.println(hours +" hours.");
+minutes = weeks * 10080;
+System.out.println(minutes +" minutes.");
+seconds = weeks * 604800;
+System.out.println(seconds +" seconds.");
 
     }
 }
